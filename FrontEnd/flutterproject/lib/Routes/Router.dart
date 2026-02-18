71cf05de8ce31e61326116996efdd4a394e38c69
mcmeowizzi-bo <mcmeowizzi@gmail.com>
2026-02-18 11:35:19 -0300

    Login page Created

--- FrontEnd/flutterproject/lib/Routes/Router.dart
+++ FrontEnd/flutterproject/lib/Routes/Router.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../pages/login.dart';
+/*import '../pages/register_page.dart';
+import '../pages/about_page.dart';
+*/import '../layouts/layout.dart';
+/*import '../pages/profile_page.dart';*/
+
 final GoRouter router = GoRouter(
   routes: [
-    GoRoute(
-      path:'/',
-      builder: (BuildContext context, GoRouterState state) => const AppLayout(child: AboutPage())
-      ),
+    /*GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) =>
+          const AppLayout(child: AboutPage()),
+    ),*/
     GoRoute(
       path: '/login',
-      builder: (BuildContext context, GoRouterState state) => AppLayout(child: LoginPage())
-      ),
-      GoRoute(
-        path: '/register',
-        builder: (BuildContext context, GoRouterState state) => AppLayout(child: RegisterPage())
-      ),
-      GoRoute(
-        path: '/main/:username',
-        builder: (BuildContext context, GoRouterState state) => {
-          final String username = state.pathparameters['username']!;
-          return AppLayout(child: HomePage());
-        }
-      )
-      
-  ]
-);  
+      builder: (BuildContext context, GoRouterState state) =>
+        AppLayout(child: LoginPage()),
+    )/*,
+    GoRoute(
+      path: '/register',
+      builder: (BuildContext context, GoRouterState state) =>
+          AppLayout(child: RegisterPage()),
+    ),
+    GoRoute(
+      path: '/profile/:username',
+      builder: (BuildContext context, GoRouterState state) {
+        final String username = state.pathParameters['username']!;
+        return AppLayout(child: ProfilePage(username: username));
+      },
+    ),*/
+  ],
+);
